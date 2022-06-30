22df980c3384d499296c0204a3a1891833e7fb53
thays <thayscasado@hotmail.com>
2022-06-30 10:28:39 -0700

    added text change

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const body = document.querySelector('body');
+
+let h1 = document.createElement('h1');
+h1.textContent = 'Welcome to Theme Switcher Challenge';
+h1.classList.add('text');
+body.appendChild(h1);
+
 let theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 // Switcher
 const switcher = document.getElementById('app');
@@ -23,7 +29,7 @@ moon.addEventListener('click', () => {
 const sun = document.createElement('img');
 sun.src = './public/images/sun.svg';
 sun.className = "sun";
-sun.addEventListener('click', () => {  setThemeMode('light'); });
+sun.addEventListener('click', () => { setThemeMode('light'); });
 
 
 // circle
@@ -43,13 +49,15 @@ const setThemeMode = (theme) => {
     if (theme === 'light') {
         circle.style.left = '7.25rem';
         body.style.backgroundColor = '#F1F1F1';
+        h1.classList.add('text-dark');
+        h1.classList.remove('text-light');
         localStorage.setItem('theme', 'light');
-        console.log(localStorage.getItem('theme'));
     } else {
         circle.style.left = '0.25rem';
         body.style.backgroundColor = '#292C35';
+        h1.classList.add('text-light');
+        h1.classList.remove('text-dark');
         localStorage.setItem('theme', 'dark');
-        console.log(localStorage.getItem('theme'));
     }
 }
 
